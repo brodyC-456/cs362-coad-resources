8728bbcb2ef3febbf0b7f367762a9cda2e9b5f2a
Brody Couture <brodycouture05@gmail.com>
2026-01-15 20:33:16 -0800

    finished attribute tests organization, region, and resource category

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-   
+  let (:organization) {Organization.new}
+
+  it "responds to name" do
+    expect(organization).to respond_to(:name)
+  end
+
+  it "responds to status" do
+    expect(organization).to respond_to(:status)
+  end
+  
+  it "responds to phone" do
+    expect(organization).to respond_to(:phone)
+  end
+
+  it "responds to email" do
+    expect(organization).to respond_to(:email)
+  end
+
+  it "responds to description" do
+    expect(organization).to respond_to(:description)
+  end
+
+  it "responds to rejection_reason" do
+    expect(organization).to respond_to(:rejection_reason)
+  end
+
+  it "responds to liability_insurance" do
+    expect(organization).to respond_to(:liability_insurance)
+  end
+  
+  it "responds to primary_name" do
+    expect(organization).to respond_to(:primary_name)
+  end
+
+  it "responds to secondary_name" do
+    expect(organization).to respond_to(:secondary_name)
+  end
+
+  it "responds to secondary_phone" do
+    expect(organization).to respond_to(:secondary_phone)
+  end
+
+  it "responds to title" do
+    expect(organization).to respond_to(:title)
+  end
+
+  it "responds to transportation" do
+    expect(organization).to respond_to(:transportation)
+  end
+
+
 end
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    let (:resource_category) {ResourceCategory.new}
+
+    it "responds to name" do
+        expect(resource_category).to respond_to(:name)
+    end
+    it "responds to active" do
+        expect(resource_category).to respond_to(:active)
+    end
 end
