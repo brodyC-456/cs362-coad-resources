646894787be98e76abe95c82e39d8f9a66e5dcec
Brody Couture <brodycouture05@gmail.com>
2026-01-15 17:05:19 -0800

    example tests from in class

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+    it "exists" do
+        Ticket.new
+    end
+
+    let (:ticket) {Ticket.new}
+
+    it "responds to name" do
+        expect(ticket).to respond_to(:name)
+    end
+
+    it "belongs to region" do
+        should belong_to(:region) # if the belongs_to is optionial, add ".optional"
+    end
+
+    # look into rspec matchers to find tests for assoc. types that aren't belongs_to
+
+    
 
 end
