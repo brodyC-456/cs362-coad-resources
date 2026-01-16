fa6d4c254bc855e381cb264f9242f4050aae5ef4
Hunter Scholz <scholzh@oregonstate.edu>
2026-01-15 20:32:00 -0800

    2.4 user and ticket spec tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -8,6 +8,25 @@ RSpec.describe Ticket, type: :model do
         expect(ticket).to respond_to(:name)
     end
 
+    it "responds to description" do
+        expect(ticket).to respond_to(:description)
+    end
+
+    it "responds to phone" do
+        expect(ticket).to respond_to(:phone)
+    end
+
+    it "responds to closed" do
+        expect(ticket).to respond_to(:closed)
+    end
+
+    it "responds to closed_at" do
+        expect(ticket).to respond_to(:closed_at)
+    end
+
+
+
+
     it "belongs to region" do
         should belong_to(:region) # if the belongs_to is optionial, add ".optional"
     end
