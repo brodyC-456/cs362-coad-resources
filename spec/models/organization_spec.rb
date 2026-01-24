86cecc9c2b2d828012193d12258f1d1a94d31498
Eric Orr <155666503+EricOrrDev@users.noreply.github.com>
2026-01-23 17:07:47 -0800

    moved all the organziation validation tests into one describes block

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,56 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
   let (:organization) {Organization.new}
-
-  describe "Validation Tests" do
-    it "responds to name" do
-      expect(organization).to respond_to(:name)
-    end
-    it "responds to status" do
-      expect(organization).to respond_to(:status)
-    end
-    
-    it "responds to phone" do
-      expect(organization).to respond_to(:phone)
-    end
-    it "responds to email" do
-      expect(organization).to respond_to(:email)
-    end
-    it "responds to description" do
-      expect(organization).to respond_to(:description)
-    end
-    it "responds to rejection_reason" do
-      expect(organization).to respond_to(:rejection_reason)
-    end
-    it "responds to liability_insurance" do
-      expect(organization).to respond_to(:liability_insurance)
-    end
-    
-    it "responds to primary_name" do
-      expect(organization).to respond_to(:primary_name)
-    end
-    it "responds to secondary_name" do
-      expect(organization).to respond_to(:secondary_name)
-    end
-    it "responds to secondary_phone" do
-      expect(organization).to respond_to(:secondary_phone)
-    end
-    it "responds to title" do
-      expect(organization).to respond_to(:title)
-    end
-    it "responds to transportation" do
-      expect(organization).to respond_to(:transportation)
-    end
-    it "has many users" do
-      should have_many(:users)
-    end
-    it "has many tickets" do
-      should have_many(:tickets)
-    end
-    it "has and belongs to many resource categories" do
-      should have_and_belong_to_many(:resource_categories)
-    end
-  end
   
   describe "Unit Tests" do
         let(:local_fire_department) do
@@ -114,6 +64,53 @@ RSpec.describe Organization, type: :model do
   # Validation Tests
   describe "validation tests" do
     let (:organization) {Organization.new}
+    it "responds to name" do
+      expect(organization).to respond_to(:name)
+    end
+    it "responds to status" do
+      expect(organization).to respond_to(:status)
+    end
+    
+    it "responds to phone" do
+      expect(organization).to respond_to(:phone)
+    end
+    it "responds to email" do
+      expect(organization).to respond_to(:email)
+    end
+    it "responds to description" do
+      expect(organization).to respond_to(:description)
+    end
+    it "responds to rejection_reason" do
+      expect(organization).to respond_to(:rejection_reason)
+    end
+    it "responds to liability_insurance" do
+      expect(organization).to respond_to(:liability_insurance)
+    end
+    
+    it "responds to primary_name" do
+      expect(organization).to respond_to(:primary_name)
+    end
+    it "responds to secondary_name" do
+      expect(organization).to respond_to(:secondary_name)
+    end
+    it "responds to secondary_phone" do
+      expect(organization).to respond_to(:secondary_phone)
+    end
+    it "responds to title" do
+      expect(organization).to respond_to(:title)
+    end
+    it "responds to transportation" do
+      expect(organization).to respond_to(:transportation)
+    end
+    it "has many users" do
+      should have_many(:users)
+    end
+    it "has many tickets" do
+      should have_many(:tickets)
+    end
+    it "has and belongs to many resource categories" do
+      should have_and_belong_to_many(:resource_categories)
+    end
 
     it ("must have an email") {should validate_presence_of(:email)}
     it ("must have a name") {should validate_presence_of(:name)}
