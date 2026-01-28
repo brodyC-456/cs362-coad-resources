6b79437e8056dc692f0772a45d3fc371ea7a423e
Brody Couture <brodycouture05@gmail.com>
2026-01-28 13:36:45 -0800

    moved the helper tests for dashboard out of user and into the correct spot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,41 +57,4 @@ RSpec.describe User, type: :model do
       expect(default_user.to_s).to eq("HuntersMom@aol.com")
     end
   end
-
-  describe "helper function tests" do
-    include DashboardHelper
-
-    # Setting up example users
-    let(:admin_user) {User.new(role: :admin)}
-
-    let(:organization_submitted_user) do
-      submitted_org = Organization.new(status: :submitted)
-      user = User.new(organization: submitted_org)
-      
-    end
-
-    let(:organization_approved_user) do
-      approved_org = Organization.new(status: :approved)
-      user = User.new(organization: approved_org)
-    end
-
-    let(:regular_user) {User.new}
-
-    # Actual Tests
-    it "dashboard helper returns admin_dashboard if user is an admin" do
-      expect(dashboard_for(admin_user)).to eq('admin_dashboard')
-    end
-
-    it "dashboard helper returns organization_submitted_dashbord if user has an organization with status submitted" do
-      expect(dashboard_for(organization_submitted_user)).to eq('organization_submitted_dashboard')
-    end
-
-    it "dashboard helper returns organization_approved_dashbord if user has an organization with status approved" do
-      expect(dashboard_for(organization_approved_user)).to eq('organization_approved_dashboard')
-    end
-    
-    it "dashboard helper returns create_application_dashboard' if the user has no active organization application and is not an admin" do
-      expect(dashboard_for(regular_user)).to eq('create_application_dashboard')
-    end
-  end
 end
