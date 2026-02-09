c065c870adae91a9e0433badd565ba20a3514922
Brody Couture <brodycouture05@gmail.com>
2026-02-09 14:48:43 -0800

    added example tests to region controller spec and updated user factory

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  describe 'as a logged out user' do
+    let (:user) { create(:user)}
+
+    it "index redirects to new user session when logged out"{
+      expect(get(:index)).to redirect_to new_user_session_path
+    }
+
+  
+  describe 'as a logged in user' do
+    let (:user) { create(:user)}
+    before (:each) {sign_in user}
+
+    it 'index redirects to dashboard'{
+        expect(get(:index)).to redirect_to dashboard_path
+    }
+  end
+
+  describe 'as an admin' do
+    let (:user) { create(:user, :admin)}
+    before (:each) {sign_in user}
+
+    it "index is successful"{
+        expect(get(:index)).to be_successful
+    }
+  end
+
 end
--- spec/factories/region_factory.rb
+++ spec/factories/region_factory.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
-  
   factory :region do
     name { "Fake region name" }
   end
