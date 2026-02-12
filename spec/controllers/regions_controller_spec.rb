c75de991f61cee4120dcf1a4fd2736241d12aa95
Brody Couture <brodycouture05@gmail.com>
2026-02-12 10:34:36 -0800

    finished region controller spec

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -4,57 +4,138 @@ RSpec.describe RegionsController, type: :controller do
 
   describe 'as a logged out user' do
     let (:user) { create(:user)}
+    let (:region) {create(:region)}
 
+    # Index
     it {
       expect(get(:index)).to redirect_to new_user_session_path
     }
 
+    # Show
+    it {
+        get(:show, params: {id: region.id})
+        expect(response).to redirect_to new_user_session_path
+    }
+
+    # New
     it {
       expect(get(:new)).to redirect_to new_user_session_path
     }
 
+    # Create
     it {
       post(:create, params: {region: FactoryBot.attributes_for(:region)})
       expect(response).to redirect_to new_user_session_path
+    }
+
+    # Edit
+    it {
+        get(:edit, params: {id: region.id})
+        expect(response).to redirect_to new_user_session_path
+    }
+
+    # Update
+    it {
+        patch(:update, params: {id: region.id, region: FactoryBot.attributes_for(:region)})
+        expect(response).to redirect_to new_user_session_path
+    }
+
+    # Destroy
+    it {
+        delete(:destroy, params: {id: region.id})
+        expect(response).to redirect_to new_user_session_path
     }
   end
   
   describe 'as a logged in user' do
     let (:user) { create(:user)}
+    let (:region) {create(:region)}
     before (:each) {sign_in user}
 
+    # Index
     it {
       expect(get(:index)).to redirect_to dashboard_path
     }
 
+    # Show
+    it {
+        get(:show, params: {id: region.id})
+        expect(response).to redirect_to dashboard_path
+    }
+
+    # New
     it {
       expect(get(:new)).to redirect_to dashboard_path
     }
 
+    # Create
     it {
       post(:create, params: {region: FactoryBot.attributes_for(:region)})
       expect(response).to redirect_to dashboard_path
     }
 
+    # Edit
+    it {
+        get(:edit, params: {id: region.id})
+        expect(response).to redirect_to dashboard_path
+    }
+
+    # Update
+    it {
+        patch(:update, params: {id: region.id, region: FactoryBot.attributes_for(:region)})
+        expect(response).to redirect_to dashboard_path
+    }
+
+    # Destroy
+    it {
+        delete(:destroy, params: {id: region.id})
+        expect(response).to redirect_to dashboard_path
+    }
   end
 
   describe 'as an admin' do
     let (:user) { create(:user, :admin)}
+    let (:region) {create(:region)}
     before (:each) {sign_in user}
 
+    # Index
     it {
       expect(get(:index)).to be_successful
     }
 
+    # Show
+    it {
+        get(:show, params: {id: region.id})
+        expect(response).to be_successful
+    }
+
+    # New
     it {
         expect(get(:new)).to be_successful
     }
 
+    # Create
     it {
       post(:create, params: {region: FactoryBot.attributes_for(:region)})
       expect(response).to redirect_to regions_path
     }
 
-    
+    # Edit
+    it {
+        get(:edit, params: {id: region.id})
+        expect(response).to be_successful
+    }
+
+    # Update
+    it {
+        patch(:update, params: {id: region.id, region: FactoryBot.attributes_for(:region)})
+        expect(response).to redirect_to @region
+    }
+
+    # Destroy
+    it {
+        delete(:destroy, params: {id: region.id})
+        expect(response).to redirect_to regions_path
+    }
   end
 end
