956968599042f75ed21ee9dfd18b46d0a0814c08
Hunter Scholz <scholzh@oregonstate.edu>
2026-02-12 17:34:49 -0800

    ticket and resource category controller specs complete

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,106 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  # index, show, new, create, edit, update and destroy
 
+  describe 'as a logged out user' do
+    let (:user) {create(:user)}
+    let (:resource_category) {create(:resource_category)}
+
+    # Index
+    it {
+      expect(get(:index)).to redirect_to new_user_session_path
+    }
+
+    # New
+    it {
+      expect(get(:index)).to redirect_to new_user_session_path
+    }
+
+    # Create
+    it {
+      post(:create, params: {resource_category: FactoryBot.attributes_for(:resource_category)})
+      expect(response).to redirect_to new_user_session_path
+    } 
+
+    # Update
+    it {
+      patch(:update, params: {id: resource_category.id, resource_category: FactoryBot.attributes_for(:resource_category)})
+      expect(response).to redirect_to new_user_session_path
+    }
+
+    # Destroy
+    it {
+      delete(:destroy, params: {id: resource_category.id})
+      expect(response).to redirect_to new_user_session_path
+    }
+  end
+
+  describe 'as a logged in user' do
+    let (:user) {create(:user)}
+    let (:resource_category) {create(:resource_category)}
+    before (:each) {sign_in user}
+
+    # Index
+    it {
+      expect(get(:index)).to redirect_to dashboard_path
+    }
+
+    # New
+    it {
+      expect(get(:index)).to redirect_to dashboard_path
+    }
+
+    # Create
+    it {
+      post(:create, params: {resource_category: FactoryBot.attributes_for(:resource_category)})
+      expect(response).to redirect_to dashboard_path
+    } 
+
+    # Update
+    it {
+      patch(:update, params: {id: resource_category.id, resource_category: FactoryBot.attributes_for(:resource_category)})
+      expect(response).to redirect_to dashboard_path
+    }
+
+    # Destroy
+    it {
+      delete(:destroy, params: {id: resource_category.id})
+      expect(response).to redirect_to dashboard_path
+    }
+  end
+
+  describe 'as admin' do
+    let (:user) {create(:user, :admin)}
+    let (:resource_category) {create(:resource_category)}
+    before (:each) {sign_in user}
+
+    # Index
+    it {
+      expect(get(:index)).to be_successful
+    }
+
+    # New
+    it {
+      expect(get(:index)).to be_successful
+    }
+
+    # Create
+    it {
+      post(:create, params: {resource_category: FactoryBot.attributes_for(:resource_category)})
+      expect(response).to redirect_to resource_categories_path
+    } 
+
+    # Update
+    it {
+      patch(:update, params: {id: resource_category.id, resource_category: FactoryBot.attributes_for(:resource_category)})
+      expect(response).to redirect_to resource_category
+    }
+
+    # Destroy
+    it {
+      delete(:destroy, params: {id: resource_category.id})
+      expect(response).to redirect_to resource_categories_path
+    }
+  end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,93 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+  describe 'as a logged out user' do
+    let (:user) {create(:user)}
+    let (:ticket) {create(:ticket)}
+
+    # New
+    it {
+      expect(get(:new)).to be_successful
+    }
+
+    # Create
+    it {
+      post(:create, params: {ticket: FactoryBot.attributes_for(:ticket)})
+      expect(response).to be_successful
+    }
+
+    # This test is probably dependant on a method from a mock
+    # or somthing that hasn't yet been implemented. (current_user.admin?)
+    # # Show
+    # it {
+    #   get(:show, params: {id: ticket.id})
+    #   expect(response).to redirect_to new_user_session_path
+    # }
+
+    # Destroy
+    it {
+      delete(:destroy, params: {id: ticket.id})
+      expect(response).to redirect_to dashboard_path
+    }
+  end
+
+  describe 'as a logged in user' do
+    let (:user) {create(:user)}
+    let (:ticket) {create(:ticket)}
+    before (:each) {sign_in user}
+
+    # New
+    it {
+      expect(get(:new)).to be_successful
+    }
+
+    # Create
+    it {
+      post(:create, params: {ticket: FactoryBot.attributes_for(:ticket)})
+      expect(response).to be_successful
+    }
+
+    # Show
+    it {
+      get(:show, params: {id: ticket.id})
+      expect(response).to redirect_to dashboard_path
+    }
+
+    # Destroy
+    it {
+      delete(:destroy, params: {id: ticket.id})
+      expect(response).to redirect_to dashboard_path
+    }
+  end
+
+  describe 'as admin' do
+    let (:user) {create(:user, :admin)}
+    let (:ticket) {create(:ticket)}
+    before (:each) {sign_in user}
+
+    # New
+    it {
+      expect(get(:new)).to be_successful
+    }
+
+    # Create
+    it {
+      post(:create, params: {ticket: FactoryBot.attributes_for(:ticket)})
+      expect(response).to be_successful
+    }
+
+    # Show
+    it {
+      get(:show, params: {id: ticket.id})
+      expect(response).to be_successful
+    }
+
+    # Destroy
+    it {
+      delete(:destroy, params: {id: ticket.id})
+      expect(response).to redirect_to (dashboard_path << '#tickets')
+    }
+
+  end
+
 end
