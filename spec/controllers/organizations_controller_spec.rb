0705ebee7eb5adbaad6c04f1f938719c5a0a6178
Brody Couture <brodycouture05@gmail.com>
2026-02-23 11:33:18 -0800

    finished organization controller spec and added stubs

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,4 @@
 # Note: Show and edit are empty in the controller, so I don't think we need to test them?
-# Also not sure if i need to test reject or approve, I don't think i do but idk
 
 require 'rails_helper'
 
@@ -33,37 +32,38 @@ RSpec.describe OrganizationsController, type: :controller do
     end
 
     describe 'as logged in user' do
-      let (:user) { create(:user) }
-      let (:organization) {create(:organization)}
-      before (:each) { sign_in user }
+      let(:user) { create(:user, role: :organization) }
 
-      # Index
-      it {
-        expect(get(:index)).to be_successful
-      }
+      before do
+        sign_in user
+        # Stubbing helpers
+        allow(controller).to receive(:verify_unapproved).and_return(true)
+        allow(controller).to receive(:verify_approved).and_return(true)
+        # Stubbing Mail
+        mail_double = double(deliver_now: true)
+        allow(UserMailer).to receive(:with).and_return(double(new_organization_application: mail_double))
+      end
 
-      # New
-      it {
-        expect(get(:new)).to be_successful
-      }
+      it {expect(get(:index)).to be_successful}
+      it {expect(get(:new)).to be_successful}
 
-      # Create -- requires a stub to be written cleanly so ill skip for now
-    #   it {
-    #     post(:create, params: {organization: FactoryBot.attributes_for(:organization)})
-    #     expect(response).to be_successful
-    #   }
+      it 'creates organization' do
+        expect {post(:create, params: { organization: attributes_for(:organization) })}.to change(Organization, :count).by(1)
+        expect(user.reload.organization).to be_present
+        expect(response).to redirect_to organization_application_submitted_path
+      end
 
-      # Update
-      it {
-        patch(:update, params: {id: organization.id, organization: FactoryBot.attributes_for(:organization)})
-        expect(response).to redirect_to dashboard_path
-      }
-      
+      it 'updates organization' do
+        org = create(:organization, status: :approved)
+        patch(:update, params: {id: org.id,organization: { name: "Updated" }})
+        expect(org.reload.name).to eq("Updated")
+        expect(response).to redirect_to organization_path(id: org.id)
+      end
     end
 
     describe 'as admin' do
       let (:user) { create(:user, :admin) }
-      let (:organization) {create(:organization)}
+      let(:fake_org) {instance_double(Organization, id: 1, name: "Test Org",approve: true,reject: true,save: true)}
       before (:each) { sign_in user }
 
       # Index
@@ -84,9 +84,27 @@ RSpec.describe OrganizationsController, type: :controller do
 
       # Update
       it {
-        patch(:update, params: {id: organization.id, organization: FactoryBot.attributes_for(:organization)})
+        patch(:update, params: {id: fake_org.id, organization: FactoryBot.attributes_for(:organization)})
         expect(response).to redirect_to dashboard_path
       }
     end
+
+  describe "approve and reject logic" do
+    let (:admin) {create(:user, :admin)}
+    let (:organization) {create(:organization)}
+    before (:each) {sign_in admin}
+
+    # Approve
+    it do
+      patch(:approve, params: {id: organization.id})
+      expect(response).to redirect_to organizations_path
+    end
+
+    # Reject
+    it do
+      patch(:reject, params: {id: organization.id, organization: {rejection_reason:"invalid"}})
+      expect(response).to redirect_to organizations_path
+    end
+  end
 
 end
