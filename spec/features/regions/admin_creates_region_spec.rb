358408bcfc805deb72cd0193b8d7398254869b88
Brody Couture <brodycouture05@gmail.com>
2026-02-25 13:16:28 -0800

    added a few end to ends from class

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-
+    let (:admin) {create(:user, :admin)}
+    it 'succeeds when a name is provided' do
+        log_in_as(admin)
+        visit new_region_path
+        fill_in 'Name', with: 'FAKE'
+        click_on 'Add Region'
+        expect(current_path).to eq(regions_path)
+        expect(page.body).to have_text('FAKE')
+    end
 end
--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
+    before do
+        @region = create(:region)
+        @resource_category = create(:resource_category)
+    end
 
+    it 'can be done from the home screen' do
+        # Sim user interaction with the page
+        visit root_path
+        click_on 'Get Help'
+        fill_in 'Full Name', with: 'Test Name'
+        fill_in 'Phone Number', with: '555-555-1212'
+        select @region.name, from: 'Region'
+        select @resource_category.name, from: 'Resource Category'
+        fill_in 'Description', with: 'Test description'
+        click_on 'Send this help request'
+
+        # Make sure it works
+        expect(current_path).to eq ticket_submitted_path
+    end
 end
